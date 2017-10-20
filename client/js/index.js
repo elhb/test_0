233dc9a5b718ed7549a405f643a17a05e62e49fe
eborgstrm <eriklhb@gmail.com>
2017-10-21 00:22:39 +0200

    vue testing

--- client/js/index.js
+++ client/js/index.js
@@ -1,5 +1,28 @@
 // index javascript
+import Vue from 'vue'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//alert('test')
+//console.log('hej')
 
-alert('test')
-
-console.log('hej')
+new Vue({
+  el: '#app',
+  data: {
+    newTodo: '',
+    todos: [
+      { text: 'Add some todos' }
+    ]
+  },
+  methods: {
+    addTodo: function () {
+      var text = this.newTodo.trim();
+      if (text) {
+        this.todos.push({ text: text });
+        this.newTodo = '';
+      }
+    },
+    removeTodo: function (index) {
+      this.todos.splice(index, 1);
+    }
+  }
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack')
+//var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 
@@ -17,18 +18,34 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+              test: /\.css$/,
+              use: ['style-loader', 'css-loader']
+            },
+            {
+              test: /\.(jpe?g|png|gif|svg)$/,
+              use: ['file-loader?name=images/[name].[ext]']
+            },
+            {
+              test: /\.(woff|woff2|eot|ttf|otf)$/,
+              use: ['file-loader?name=fonts/[name]-[hash:6].[ext]']
             }
-
         ]
     },
 
     plugins: [
-                new HtmlWebpackPlugin(
-                    {
-                        filename: 'index.html',
-                        title: 'test application 0'
-                    }
-                )
-            ]
-
+//            new HtmlWebpackPlugin(
+//                {
+//                    filename: 'index.html',
+//                    title: 'test application 0'
+//                }
+//            )
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            Popper: ['popper.js', 'default'],
+            })
+    ]
 }
