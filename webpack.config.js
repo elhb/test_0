198a5b07eb48ea6b8762a43acb84f9cfd60ebde7
eborgstrm <eriklhb@gmail.com>
2017-10-20 16:39:41 +0200

    and a index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 
@@ -19,5 +20,12 @@ module.exports = {
             }
 
         ]
-    }
+    },
+
+    plugins: [
+                new HtmlWebpackPlugin(
+                    title: 'test application 0'
+                )
+            ]
+
 }
